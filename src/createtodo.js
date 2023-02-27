62b909d2fa116b0572e6bc18f1f0682c8924f71e
Justin Elliott <thecrashtestjustin@gmail.com>
2023-02-27 09:16:17 -0800

    add folders as object element

--- src/createtodo.js
+++ src/createtodo.js
@@ -1,9 +1,10 @@
-export const toDo = (title, dueDate, dueTime, description) => {
+export const toDo = (title, dueDate, dueTime, description, folders) => {
   return {
     title: title,
     dueDate: dueDate,
     dueTime: dueTime,
     description: description,
+    folders: folders,
     complete: false,
   };
 };
